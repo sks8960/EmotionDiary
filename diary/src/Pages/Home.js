3cfd96758cc8d936ffa99414f051f7cf88506b80
sks8960 <sks8960@naver.com>
2023-08-26 17:02:31 +0900

    home page 구현

--- diary/src/Pages/Home.js
+++ diary/src/Pages/Home.js
@@ -1,7 +1,55 @@
+import { useState, useContext, useEffect } from "react";
+import MyHeader from './../components/MyHeader';
+import MyButton from './../components/MyButton';
+import { DiaryStateContext } from "../App";
+import DiaryList from "./../components/DiaryList";
+
 const Home = () => {
-    return <div>
-        <h1>home</h1>
-        <p>이곳은 HOME입니다.</p>
-    </div>
+
+    const diaryList = useContext(DiaryStateContext)
+    const [data, setData] = useState([]);
+    const [curDate, setcurDate] = useState(new Date());
+
+    useEffect(() =>{
+        if(diaryList.length >= 1){
+            const firstDay = new Date(
+                curDate.getFullYear(),
+                curDate.getMonth(),
+                1
+            ).getTime();
+            const lastDay = new Date(
+                curDate.getFullYear(),
+                curDate.getMonth() + 1,
+                0
+            ).getTime();
+            
+            setData(diaryList.filter((it)=> firstDay <= it.date && it.date <= lastDay))
+        }
+    },[diaryList, curDate]);
+
+    useEffect(()=> {
+        console.log(data);
+    }, [data]);
+
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월  `
+
+    const increaseMonth = () => {
+        setcurDate(new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate()));
+    }
+
+    const decreaseMonth = () => {
+        setcurDate(new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate()));
+    }
+
+    return (
+        <div>
+            <MyHeader 
+                headText={headText}
+                leftChild ={<MyButton text={"<"} onClick={decreaseMonth}/>}
+                rightChild ={<MyButton text={">"} onClick={increaseMonth}/>}
+            />
+            <DiaryList diaryList = {data} />
+        </div>
+    );
 }
 export default Home;
